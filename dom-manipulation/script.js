4a0f0998c7472a4d2f6af29363c5c7ef3073837b
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2025-07-14 11:29:06 +0300

    0. Building a Dynamic Content Generator with Advanced DOM Manipulation

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,4 +1,29 @@
 
-// array object where each object has test and category
-let quote = []
 
+
+// array object where each object has test and category
+let quotes = [
+    { text: "Life is what happens when you're busy making other plans.", category: "Life" },
+    { text: "The only way to do great work is to love what you do.", category: "Work" },
+    { text: "Be yourself; everyone else is already taken.", category: "Inspiration" },
+  ];
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Show random initial quote
+    showRandomQuote() 
+
+    
+})
+
+const showRandomQuote = () => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const randomQuote = quotes[randomIndex];
+
+    document.getElementById("quoteDisplay").innerText = randomQuote.text;
+}
+
+const buttonNewQoute = document.getElementById("Button-newQuote")
+const handleClickNewQoute = () => {
+    console.log("Button was clicked")
+    
+} 
